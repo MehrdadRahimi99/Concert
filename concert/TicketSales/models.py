b44fc1b7de0ca4320905c9c9f1c18edb4b6c5256
Mehrdad <rms.rahimi@gmail.com>
2022-11-22 12:12:13 +0330

    feat: append new class
    class timemodel & append some features to previous models

--- concert/TicketSales/models.py
+++ concert/TicketSales/models.py
@@ -10,10 +10,17 @@ class consertmodel (models.Model):
         return self.SingerName
     
 class locationmodel (models.Model):
+    IdNumbers = models.IntegerField(primary_key=True)
     Name = models.CharField(max_length=100)
-    Address = models.CharField(max_length=500)
-    Phone = models.CharField(max_length=11)
+    Address = models.CharField(max_length=500,default="تهران برج میلاد")
+    Phone = models.CharField(max_length=11, null=True)
     Capacity = models.IntegerField()
     
     def __str__(self):
         return self.Name    
+
+class timemodel(models.Model):
+    concertmodel = models.ForeignKey("to=consertmodel", on_delete=models.PROTECT)
+    locationmodel = models.ForeignKey("to=locationmodel", on_delete=models.PROTECT)
+    startdatetime = models.DateTimeField()
+    seats = models.IntegerField()
